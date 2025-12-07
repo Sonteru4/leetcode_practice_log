977e5da0e81f8ac394bd7addff7d134fa3cb2d7d
Sanjana Onteru <sanjanaonteru404@gmail.com>
2025-12-06 21:39:36 -0600

    Add 0002 problem file and clean up filenames

---
+++ problems/easy/0002_remove_duplicates_from_sorted_array.py
@@ -0,0 +1,6 @@
+#Sorted set
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> int:
+        unique = sorted(set(nums))
+        nums[:len(unique)] = unique
+        return len(unique)
